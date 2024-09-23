9bd52937343e5fea8f213225b54b3e542a02a8bb
ivankolt <va.koltsov@mail.ru>
2024-09-23 16:30:11 +0500

    Задания 4,5,6

--- ConsoleApp1bka/Program.cs
+++ ConsoleApp1bka/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,27 @@ namespace ConsoleApp1bka
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(ThreeNumber(8, 2, 2));
+            Console.WriteLine(DayOfWeek(3));
+            Console.WriteLine(Min(1, 5));
+        }
+        public static double ThreeNumber(int a, int b, int c)
+        {
+            return Convert.ToDouble((a - b)) / c;
+        }
+        public static string DayOfWeek(int a)
+        {
+
+            DateTime dateValue = new DateTime(1906, 1, a);
+            string Name = dateValue.ToString("dddd");
+
+            return Name;
+        }
+        public static int Min(int a, int b)
+        {
+            if (a < b) return a;
+
+            return b;
         }
     }
 }

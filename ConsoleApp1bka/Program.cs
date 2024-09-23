2170b3e43616c602219c1c4226ed0c96a7307fe7
Vadim axlestin <vadusbeska@gmail.com>
2024-09-23 13:36:27 +0500

    Сделал с 1-3 задание

--- ConsoleApp1bka/Program.cs
+++ ConsoleApp1bka/Program.cs
@@ -10,6 +10,38 @@ namespace ConsoleApp1bka
     {
         static void Main(string[] args)
         {
+        Double a = Convert.ToDouble(Console.ReadLine());
+        Double b = Convert.ToDouble(Console.ReadLine());
+          
+            Console.WriteLine($" куб числа равен {grizld(a)}");
+            Console.WriteLine($" квадрат числа равен {grizli2(a)}");
+            Console.WriteLine($" сумма чисел равна {grizli3(a,b)}");
+                  Console.ReadKey();
         }
+        public static double grizld (double a)
+            {
+            
+            
+               return Math.Pow(a,3);
+            }
+          public static double grizli2 (double a)
+            {
+            
+            
+               return Math.Pow(a,2);
+            }
+         public static double grizli3 (double a, double b)
+            {
+            
+            
+               return a+b;
+
+
+
+
+
+            }
+      
     }
+
 }
